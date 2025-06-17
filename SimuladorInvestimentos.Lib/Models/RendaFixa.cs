9321da3fa483e2b74fc4a07d6c6191ebd291ff55
leticia-zg <85648087+leticia-zg@users.noreply.github.com>
2025-06-17 13:15:39 -0300

    Update RendaFixa.cs

--- SimuladorInvestimentos.Lib/Models/RendaFixa.cs
+++ SimuladorInvestimentos.Lib/Models/RendaFixa.cs
@@ -1,15 +1,14 @@
-﻿namespace SimuladorInvestimentos.Lib.Models
-
-{ 
+namespace SimuladorInvestimentos.Lib.Models
+{
     public class RendaFixa : Investimento
     {
         public RendaFixa(decimal valorInvestido, int tempoMeses) : base(valorInvestido, tempoMeses)
         { }
+
         public override decimal CalcularRendimento()
         {
             decimal taxaJuros = 0.005m;
-            (1 + taxaJuros * tempoMeses;
+            return ValorInvestido * (1 + taxaJuros * TempoMeses);
         }
     }
 }
-
